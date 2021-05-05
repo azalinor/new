c8a9083b229dc0146082ab993c21266aeb59f1ff
azalinor <80928727+azalinor@users.noreply.github.com>
2021-05-05 11:30:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <div class="jumbotron text-center">
   <h1>My First Bootstrap Page</h1>
-    <h3>2 web app from same github</h3>
+    <h3>2 web app from same github. Yahoo</h3>
   
   <?php
 echo "Today is " . date("Y/m/d") . "<br>";
